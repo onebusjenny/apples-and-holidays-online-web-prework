525729b1a28b6ff7824021b2d8b2745b57af610f
Jenny Chu <jchudesign0930@gmail.com>
2019-02-20 07:00:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   
-  supply = {:season => 
+  winter_supplies(holiday_supplies)
   # code here
   # remember to return the updated hash
 
